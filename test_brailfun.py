56e36489bb2d5da734bc957c1f3bc97139af9528
Angelo Mosquera <anggelomos@outlook.com>
2020-07-31 11:17:07 -0500

    Change clamp function's test 2
    add more test cases

--- test_brailfun.py
+++ test_brailfun.py
@@ -12,11 +12,11 @@ class test_brailfun(unittest.TestCase):
             self.assertEqual(testing_cell.clamp(testcase[index]), expected[index])
 
     def test_clamp_function_02(self):
-        testcase = ["a", j, ["a"], {"a"}, ("a")]
+        testcase = ["a", ["a"], {"a"}, set("a"), ("a")]
         expected = []
         for index, _ in enumerate(testcase):
             with self.assertRaises(TypeError):
-                testing_cell.clamp(testcase[index]), expected[index]
+                testing_cell.clamp(testcase[index])
 
 if __name__ == "__main__":
     unittest.main()
